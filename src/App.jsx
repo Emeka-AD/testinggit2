8729f4cfb34e35a8ad694f4ab90c14d9a530790a
Emeka-AD <pauladingwupu@gmail.com>
2025-07-30 14:12:33 +0100

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Test from './components/Test'
+
+const URL = 'https://api.disneyapi.dev/character';
+
+function App() {
+  
+  const [characters, setCharacters] = useState([]) // 1. State for data
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // 2. Fetch data when component mounts
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.data)
+        setLoading(false)
+      }) // 3. Update state with fetched data
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <>
+      {loading ? <p>Loading...</p> : (
+        <ol>
+          {characters.map(character => (
+            <li key={character._id}>{character.name}</li>
+          ))}
+        </ol>
+      )}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>Test this is the newly entered test text</div>
+  )
+}
+
+export default Test
